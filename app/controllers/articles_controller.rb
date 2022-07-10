59f7d1d04fd8a58379a0140918cba2de9808b1f2
jiveeee <m.javad.raisi@gmail.com>
2022-07-10 16:40:53 +0200

    adds CRUD operations & makes sure it DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,55 @@
 class ArticlesController < ApplicationController
+
+    before_action :find_article, only:[:show, :edit, :update, :destroy] 
+
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
         @articles = Article.all
     end
+
+    def new 
+        @article = Article.new 
+    end
+
+    def edit
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article is created successfully"
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article is updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    # "private" should be always placed at the bottom of your syntax as anything under it will be accessible only here 
+    private
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
