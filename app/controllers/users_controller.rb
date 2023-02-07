c1b46d1de67a36ea215c2be2c38d2f45eaf670ec
Javad Raisi <javad@MBP-von-Javad.local>
2023-02-07 12:36:56 +0100

    creates the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
-class UsersControllers < ApplicationController
+class UsersController < ApplicationController
     #we already have created & migrated users table, added & migrated password_digest column to it.
     def new
         @user = User.new
     end
 
-    def Create
-        @user = User.new(user_parms)
+    def create
+        @user = User.new(user_params)
         if @user.save
             flash[:notice] = "Welcome to the Blog on Rails #{@user.username}!"
             redirect_to articles_path
