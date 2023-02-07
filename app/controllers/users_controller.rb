7b884285c870727418a5bceef6d6f3794691f815
Javad Raisi <javad@MBP-von-Javad.local>
2023-02-07 14:11:40 +0100

    adds edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
     #we already have created & migrated users table, added & migrated password_digest column to it.
+
+    before_action :find_user, only:[:edit, :update]
+
     def new
         @user = User.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Your profile is successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
     end
 
     def create
@@ -15,6 +30,10 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
