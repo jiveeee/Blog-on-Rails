d54c675ec82c441d4e2ee58b766898e5a53f2014
Javad Raisi <javad@MacBook-Pro-von-Javad.local>
2023-02-13 15:42:17 +0100

    adds helper methods to use in the views, personalizing the views for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Blog on Rails #{@user.username}!"
             redirect_to articles_path
         else
