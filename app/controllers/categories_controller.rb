7b9ac1fd0709038fe2a4b5ded5b17fa751deb510
Javad Raisi <javad@MBP-von-Javad.local>
2023-02-16 16:41:30 +0100

    enables the app to create a category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,11 +2,28 @@ class CategoriesController < ApplicationController
 
     def show
     end
-
+ 
     def index
     end
     
     def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "You successfully created a new category!"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 
 end
